cef32b567d5040cd7bd136b731e5cacd31fa7801
Mazayd <garagulya699@gmail.com>
2025-11-19 13:42:52 +0200

    feat: wrap mock response in promise and add async delay for photo metadata retrieval

--- src/modules/ai-client/ai-client.service.ts
+++ src/modules/ai-client/ai-client.service.ts
@@ -1,1 +1,23 @@
-export class AiClientService {}
+import { ICarsResponse } from './interfaces';
+
+export class AiClientService {
+    public async getPhotoMetaData(image: Express.Multer.File): Promise<ICarsResponse> {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(this.getMockCars());
+            }, 100);
+        });
+    }
+
+    private getMockCars() {
+        return {
+            cars: [
+                {
+                    plate: 'KI1111YD',
+                    raw_text: 'KI1111YD',
+                    confidence: 99.5,
+                },
+            ],
+        };
+    }
+}
---
+++ src/modules/ai-client/interfaces/cars-response.interface.ts
@@ -0,0 +1,9 @@
+export interface ICarInfo {
+    plate: string;
+    raw_text: string;
+    confidence: number;
+}
+
+export interface ICarsResponse {
+    cars: ICarInfo[];
+}
