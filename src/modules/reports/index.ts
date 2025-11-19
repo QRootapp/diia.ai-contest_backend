6fad4224f6d10b06b7b40a223afeee6da47d9213
Mazayd <garagulya699@gmail.com>
2025-11-19 11:28:03 +0200

    feat: connect aiClientService with report module

--- src/modules/reports/index.ts
+++ src/modules/reports/index.ts
@@ -6,6 +6,7 @@ import { Knex } from 'knex';
 import { ReportsPhotosRepository } from '../reports-photos/reports-photos.repository';
 import { ReportsPhotosService } from '../reports-photos/reports-photos.service';
 import { S3UploaderService } from '../s3-uploader/s3-uploader.service';
+import { AiClientService } from '../ai-client/ai-client.service';
 
 export function buildReportModule(db: Knex) {
     const reportsRepository = new ReportRepository(db);
@@ -13,8 +14,9 @@ export function buildReportModule(db: Knex) {
 
     const s3UploaderService = new S3UploaderService();
 
+    const aiClientService = new AiClientService();
     const reportsPhotosService = new ReportsPhotosService(s3UploaderService, reportsPhotosRepository);
-    const reportsService = new ReportService(reportsPhotosService, reportsRepository);
+    const reportsService = new ReportService(reportsPhotosService, reportsRepository, aiClientService);
 
     const controller = new ReportController(reportsService);
     const router = ReportRouter(controller);
--- src/modules/reports/report.service.ts
+++ src/modules/reports/report.service.ts
@@ -1,9 +1,14 @@
+import { AiClientService } from '../ai-client/ai-client.service';
 import { ReportsPhotosService } from '../reports-photos/reports-photos.service';
 import { CreateReportDto } from './dto';
 import { ReportRepository } from './report.repository';
 
 export class ReportService {
-    constructor(private readonly reportsPhotosService: ReportsPhotosService, private readonly reportRepository: ReportRepository) {}
+    constructor(
+        private readonly reportsPhotosService: ReportsPhotosService,
+        private readonly reportRepository: ReportRepository,
+        private readonly aiClientService: AiClientService
+    ) {}
 
     public async createNewReport(createReportDto: CreateReportDto, image?: Express.Multer.File) {}
 }
