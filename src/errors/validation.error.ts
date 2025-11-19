d169dc849302364bca0f2f7d669ea4e5ae046547
Mazayd <garagulya699@gmail.com>
2025-11-19 13:43:20 +0200

    feat: add errors type

--- src/errors/validation.error.ts
+++ src/errors/validation.error.ts
@@ -3,7 +3,7 @@ import { AppError } from './app.error';
 export class ValidationError extends AppError {
     public readonly errors: any[];
 
-    constructor(errors: any[]) {
+    constructor(errors: string[]) {
         super('Validation failed', 400);
         this.errors = errors;
     }
