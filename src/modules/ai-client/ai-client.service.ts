d1068b1388909859e8404f723f265d686d6dd9df
Mazayd <garagulya699@gmail.com>
2025-11-22 13:28:07 +0200

    feat: update ai-service

--- src/modules/ai-client/ai-client.service.ts
+++ src/modules/ai-client/ai-client.service.ts
@@ -1,12 +1,22 @@
+import axios from 'axios';
 import { ICarsResponse } from './interfaces';
+import FormData from 'form-data';
 
 export class AiClientService {
     public async getPhotoMetaData(image: Express.Multer.File): Promise<ICarsResponse> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(this.getMockCars());
-            }, 100);
+        const form = new FormData();
+        form.append('file', image.buffer, {
+            filename: image.originalname,
+            contentType: image.mimetype,
         });
+
+        const response: ICarsResponse = await axios.post('https://target-service.com/upload', form, {
+            headers: {
+                ...form.getHeaders(),
+            },
+        });
+
+        return response;
     }
 
     private getMockCars() {
