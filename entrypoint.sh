6612158f2ba8a63db343cc41bb538c71f09b7c03
Mazayd <garagulya699@gmail.com>
2025-11-22 09:16:49 +0200

    feat: update dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
-#!/bin/sh
-
-echo "Waiting for MySQL to be ready..."
-
-until node -e "const mysql = require('mysql2/promise'); mysql.createConnection({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS}).then(c => c.end()).catch(() => process.exit(1))"; do
-  sleep 1
-done
-
-echo "MySQL is up. Running migrations..."
-
+echo "Running migrations..."
 npm run migrate:latest
 
-echo "Starting application..."
+echo "Starting app..."
 npm run start
